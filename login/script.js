d382a643422452ac9b4f78f525500db17d19b83e
Miazak1 <vinicius.santos@sptech.school>
2024-10-14 18:24:58 -0300

    att tela de login com js

--- login/script.js
+++ login/script.js
@@ -10,3 +10,25 @@ botaoregistro.addEventListener('click', () =>{
     container.classList.remove('active');
 });
 
+const gerarSenha = document.getElementById('#senha');
+const senhaGerada= document.getElementById('#senhaGerada');
+
+var qtdSenha = 10
+var senha = ``
+
+const caracterRandom= 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklçzxcvnbm1234567890<>!@#$%&*()[]{}'
+
+function gerarSenhaa(){
+    for(cR = 1; cR <= qtdSenh; cr++ ){
+        var numerosAleatorios= Math.Floor(Math.random()* caracterRandom.length)
+
+        senha+=caracterRandom.substring(numerosAleatorios, numerosAleatorios + 1)
+
+    }
+    document.getElementById('#senha').value = senha
+}
+
+
+
+
+
