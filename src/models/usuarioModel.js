3424d9557c5a9d1fa61e82100abced54fd3562bc
Miazak1 <vinicius.santos@sptech.school>
2024-11-16 14:46:52 -0300

    integração webdataviz

---
+++ src/models/usuarioModel.js
@@ -0,0 +1,28 @@
+var database = require("../database/config")
+
+function autenticar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucaoSql = `
+        SELECT idEmpresa, nomeEmpresa, emailEmpresa, senhaEmpresa FROM Empresa WHERE emailEmpresa = '${email}' AND senhaEmpresa = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
+function cadastrar(nomeEmpresa, telefoneEmpresa, cnpj, emailEmpresa, senhaEmpresa) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, senha, telefone, cnpj, email);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO Empresa (nomeEmpresa, senhaEmpresa, telefoneEmpresa, cpnjEmpresa, emailEmpresa) VALUES ('${nomeEmpresa}', '${telefoneEmpresa}', '${cnpj}', '${emailEmpresa}' '${senhaEmpresa}' );
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    autenticar,
+    cadastrar
+};
